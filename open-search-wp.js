67add35f91574db3b3697845a469904619fe34e8
Kyle B. Johnson <kyle@wpninjas.com>
2016-01-31 23:11:16 -0500

    create barebones version for customiation.

--- open-search-wp.js
+++ open-search-wp.js
@@ -6,44 +6,56 @@ jQuery(function() {
         jQuery( '#oswp_search_results' ).remove();
         jQuery( '#oswp_search_searching' ).remove();
 
+        // Get the search Input
         var searchInput = jQuery( this );
 
+        // If the input value is empty, exit search.
+        if( ! searchInput.val() ) {
+            jQuery( '#oswp_search_searching' ).remove();
+            return;
+        }
+
+        // Append "Searching" text
         searchInput.after( '<p id="oswp_search_searching" style="margin: 0; padding: 10px; white-space: nowrap; overflow: hidden; background-color: white;">Searching...</p>' );
 
         var oswp_search = {
             action: 'oswp_search',
-            //security: '<?php echo wp_create_nonce( 'ninja_forms_ajax_nonce' ); ?>',
             okpreps_search_term: searchInput.val()
         };
 
-        if( ! searchInput.val() ) {
-
-            jQuery( '#oswp_search_searching' ).remove();
-
-            return;
-        }
-
+        // AJAX Request
         jQuery.post(ajaxurl, oswp_search, function (response) {
 
+            // Parse the response form the server.
             var response = JSON.parse( response );
 
+            // Format wrapper for the results output.
+            // TODO: Customize display wrapper.
             var resultHTML = jQuery( '<ul id="oswp_search_results" style="margin: 0; padding: 10px; white-space: nowrap; overflow: hidden; background-color: white; border-right: 10px solid white;"></ul>' );
 
+            // Initialize marker for seperating post types.
             var last_type = '';
 
-            if( 0 != jQuery( response.results).length ) {
+            if( 0 != jQuery( response.results ).length ) {
 
                 jQuery.each(response.results, function (index, value) {
 
                     if (value.type != last_type) {
+
+                        // Append header for new post type divider.
+                        // TODO: Customize display format.
                         resultHTML.append('<li><strong>' + value.type + '</strong></li>');
                         last_type = value.type;
                     }
 
-                    //resultHTML.append('<li>' + value.site + ' - <a href="' + value.href + '">' + value.text + '</a></li>');
+                    // Append result.
+                    // TODO: Customize display format.
                     resultHTML.append('<li><a href="' + value.href + '">' + value.text + '</a></li>');
                 });
             } else {
+
+                // No results were found.
+                // TODO: Customize empty results display.
                 resultHTML = jQuery( '<p id="oswp_search_results" style="margin: 0; padding: 10px; white-space: nowrap; overflow: hidden; background-color: white;">No Results Found.</p>' );
             }
 
@@ -51,12 +63,16 @@ jQuery(function() {
             jQuery( '#oswp_search_results' ).remove();
             jQuery( '#oswp_search_searching' ).remove();
 
+            // Append Search Results Output.
             searchInput.after( resultHTML );
 
+            // DEBUG: Log server response.
             console.log( response );
         });
 
-    }, 500 );
+    }, 500 ); // debounce keyup listender by 500 milliseconds.
 
+    // Bind KeyUp Listender to an element ont the page.
+    // TODO: Attach to appropriate search box.
     jQuery('[name="s"]').keyup( OpenSearchWPKeyUp );
 });
--- open-search-wp.php
+++ open-search-wp.php
@@ -34,55 +34,14 @@ final class OpenSearchWP
 
     public function search_results()
     {
-        global $wpdb;
-
-        $query = array();
-
-        if( is_multisite() ) {
-            foreach (wp_get_sites() as $site) {
-
-                switch_to_blog($site['blog_id']);
-
-                $query[] = "SELECT DISTINCT * FROM {$wpdb->posts} WHERE `post_title` LIKE '%{$this->_search}%' AND `post_status` = 'publish'";
-
-                restore_current_blog();
-            }
-        } else {
-            $query[] = "SELECT DISTINCT * FROM {$wpdb->posts} WHERE `post_title` LIKE '%{$this->_search}%' AND `post_status` = 'publish'";
-        }
-
-        $results = $wpdb->get_results( implode( ' UNION ', $query ) );
-
-        usort( $results, array( $this, 'compare_post_types' ) );
-
-        $count = array();
+        $results = array();
 
         foreach( $results as $result ){
-
-            if( ! in_array( $result->post_type, $this->_post_types ) ) continue;
-
-            if( isset( $count[ $result->post_type ] ) && 10 <= $count[ $result->post_type ] ) continue;
-
-            if( is_multisite() ) {
-                $site = ucwords(end(array_reverse(explode('.', str_replace(array('http://', 'https://'), '', str_replace('-', ' ', $result->guid))))));
-            } else {
-                $site = site_url();
-            }
-
-            $post_type = ucwords( str_replace( array( 'sp_', 'okpreps_' ), '', $result->post_type ) ) . 's';
-
-            $href = $result->guid;
-
             $this->_results[] = array(
-                'site' => $site,
-                'type' => $post_type,
-                'text' => $result->post_title,
-                'href' => $href
+                'type' => '', // Post Type
+                'text' => '', // Display Text
+                'href' => '' // Hyperlink
             );
-
-            if( ! isset( $count[ $result->post_type ] ) ) $count[ $result->post_type ] = 0;
-
-            $count[ $result->post_type ] += 1;
         }
 
         $this->_respond();
@@ -95,11 +54,6 @@ final class OpenSearchWP
         wp_enqueue_script( 'oswp' );
 
         wp_localize_script( 'oswp', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
-    }
-
-    private function compare_post_types( $a, $b )
-    {
-        return strcmp( $a->post_type, $b->post_type );
     }
 
     private function _respond()
